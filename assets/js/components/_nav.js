a3a77d60fcfb4f1bfad634322519896458cf5f69
Raúl Aguilar <soy.raulaguilar@gmail.com>
2016-03-06 04:58:08 -0600

    New components and Smooth Scrolling
    New Jade components and Smooth Scrolling files

--- assets/js/components/_nav.js
+++ assets/js/components/_nav.js
@@ -1,3 +1,29 @@
-$('.nav').on('click', function(){
-  $(this).css('background-color', 'red');
+$('.navToggle').on('click', function(e){
+  e.stopPropagation();
+  $(this).toggleClass('isActive');
+  $('.navPrimary').toggleClass('isActive');
+});
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        $('.navToggle').removeClass('isActive');
+        $('.navPrimary').removeClass('isActive');
+        return false;
+
+      }
+    }
+  });
+});
+
+$('.goToDown').on('click', function(){
+  $('html, body').animate({
+    scrollTop: viewportHeight
+  }, 1000);
 });
