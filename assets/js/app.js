b9335c2a35e4f23b324eeac97f6a0a79282642c5
Raúl Aguilar <soy.raulaguilar@gmail.com>
2016-03-05 20:30:38 -0600

    Header intro
    Markup & CSS in Header intro component
    Upload JS file when windows resize

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,18 @@
 // @codekit-append "components/_nav.js";
+
+
+// vars
+var viewportWidth = window.innerWidth;
+var viewportHeight = window.innerHeight;
+
+$(document).ready(function(){
+
+  // When viewportHeight resize.
+
+  $(window).resize(function() {
+    viewportWidth = window.innerWidth;
+    viewportHeight = window.innerHeight;
+    $('#headerIntro').css('height', viewportHeight);
+  });
+
+});
